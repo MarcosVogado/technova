0480ba3551d528ccbff9735d0a50f069594a1771
JoaoCesarDev22 <joaocesarnettocastro@gmail.com>
2026-05-12 23:55:22 -0300

    feat: endpoints de relatórios e interface web com visual neon

--- core/serializers.py
+++ core/serializers.py
@@ -1,28 +1,67 @@
+import re
 from rest_framework import serializers
 from .models import Cliente, Produto, Venda, ItemVenda
 
+# --- CLIENTE ---
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
 
+    def validate_cpf(self, value):
+        cpf_limpo = re.sub(r'\D', '', value)
+        if len(cpf_limpo) != 11:
+            raise serializers.ValidationError('CPF deve conter 11 dígitos.')
+        return value
+
+# --- PRODUTO ---
 class ProdutoSerializer(serializers.ModelSerializer):
+    estoque_baixo = serializers.ReadOnlyField()
+
     class Meta:
         model = Produto
-        fields = '__all__'
+        fields = [
+            'id', 'nome', 'descricao', 'categoria', 'preco',
+            'quantidade_estoque', 'estoque_minimo', 'ativo',
+            'estoque_baixo',
+        ]
 
+# --- ITENS DA VENDA ---
 class ItemVendaSerializer(serializers.ModelSerializer):
-    # Mostra o nome do produto ao invés de só o ID
     produto_nome = serializers.ReadOnlyField(source='produto.nome')
 
     class Meta:
         model = ItemVenda
-        fields = ['id', 'produto', 'produto_nome', 'quantidade', 'preco_unitario']
+        fields = ['id', 'produto', 'produto_nome', 'quantidade', 'preco_unitario', 'subtotal']
+        read_only_fields = ['preco_unitario', 'subtotal']
 
+class ItemVendaWriteSerializer(serializers.Serializer):
+    """Auxiliar para o processo de escrita"""
+    produto = serializers.IntegerField()
+    quantidade = serializers.IntegerField(min_value=1)
+
+# --- VENDA (LEITURA) ---
 class VendaSerializer(serializers.ModelSerializer):
     itens = ItemVendaSerializer(many=True, read_only=True)
     cliente_nome = serializers.ReadOnlyField(source='cliente.nome')
+    usuario_nome = serializers.ReadOnlyField(source='usuario.username')
 
     class Meta:
         model = Venda
-        fields = ['id', 'cliente', 'cliente_nome', 'data_venda', 'total', 'itens']
+        fields = [
+            'id', 'data_venda', 'cliente', 'cliente_nome',
+            'usuario', 'usuario_nome', 'valor_total',
+            'observacoes', 'itens',
+        ]
+        read_only_fields = ['valor_total', 'usuario']
+
+# --- VENDA (ESCRITA - O QUE ESTAVA FALTANDO!) ---
+class VendaCreateSerializer(serializers.Serializer):
+    cliente = serializers.IntegerField()
+    observacoes = serializers.CharField(required=False, allow_blank=True, default='')
+    itens = ItemVendaWriteSerializer(many=True)
+
+    def validate_itens(self, value):
+        if not value:
+            raise serializers.ValidationError('A venda deve conter pelo menos um item.')
+        return value
